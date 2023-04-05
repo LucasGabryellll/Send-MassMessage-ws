eba7b4c6f4a5b47f64730c572d0b0bb826fa4cee
LucasGabryellll <gabryelmonteiro1@gmail.com>
2023-04-04 23:13:44 -0300

    Api

---
+++ src/utils/itemsInterval.ts
@@ -0,0 +1,32 @@
+export const selectItems = [
+  {
+    id: 1,
+    description: "20s",
+    value: 20
+  },
+  {
+    id: 2,
+    description: "30s",
+    value: 30
+  },
+  {
+    id: 3,
+    description: "40s",
+    value: 40
+  },
+  {
+    id: 4,
+    description: "1 min",
+    value: 60
+  },
+  {
+    id: 5,
+    description: "90s",
+    value: 90
+  },
+  {
+    id: 6,
+    description: "2 min",
+    value: 120
+  },
+]
