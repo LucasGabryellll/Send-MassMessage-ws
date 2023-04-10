e9efa586edacb0d7cc247824aa1132e8b29917f0
LucasGabryellll <gabryelmonteiro1@gmail.com>
2023-04-10 17:47:34 -0300

    criando componente de Submit

--- src/components/ButtonSubmit/index.tsx
+++ src/components/ButtonSubmit/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Loading } from "../Loading";
 
 type Props = {
-  onPress: () => {},
+  onPress: () => void
   isLoading: boolean,
 
   message: string,
@@ -26,15 +26,15 @@ export function ButtonSubmit({ onPress, isLoading = false, message, interval }: 
     }
 
     else {
-      return onPress();
+      onPress();
     }
   } 
 
   return (
     <button
       onClick={valitadedForm}
-      className='btn btn-lg pl-20 pr-20 mt-6 md:mt-10 mb-2'
-      disabled={isLoading || message === "" || interval > 0}
+      className='btn btn-lg pl-20 pr-20 mt-6 md:mt-10 mb-2 cursor-pointer'
+      disabled={isLoading || message === "" || interval < 0}
     >
       {isLoading ? <Loading /> : "INICIAR ENVIO"}
     </button>
