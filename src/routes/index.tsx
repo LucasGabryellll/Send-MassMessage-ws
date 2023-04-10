77b579b1fe4fd00fc3f07074423caf6e2f6b3fbc
LucasGabryellll <gabryelmonteiro1@gmail.com>
2023-04-10 17:50:06 -0300

    Criando rotas privadas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 
+import Login from '../pages/Login';
 import { Home } from '../pages/Home';
 import { SendContact } from '../pages/SendContact';
 import { SendImage } from '../pages/SendImage';
 
+import { AuthContext } from '../context/auth';
+
 export function AppRouter() {
+  const userContext = useContext(AuthContext);
+
+  const PrivateRouter = () => {
+
+    if (userContext?.isLogged) {
+      return <Outlet />;
+    }
+
+    return <Login />;
+  }
+
   return (
     <Router>
       <Routes>
-        <Route index path="/" element={<Home />} />
-
-        <Route path="/message/massText" element={<SendContact />} />
-
-        <Route path="/message/massImage" element={<SendImage />} />
+        <Route element={<PrivateRouter />} >
+          <Route path='/' element={<Home/>} />
+          <Route path='/message/massText' element={<SendContact />} />
+          <Route path='/message/massImage' element={<SendImage />}/>
+        </Route>
       </Routes>
     </Router>
   );
