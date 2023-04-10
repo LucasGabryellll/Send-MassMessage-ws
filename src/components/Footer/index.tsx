1ef0c79a4ae9640b6cde53a589cd3f29a4eda41d
LucasGabryellll <gabryelmonteiro1@gmail.com>
2023-04-10 17:47:45 -0300

    Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function Footer() {
+  return (
+    <footer
+      className="fixed bottom-0 w-full text-center 
+    text-[12px] md:text-[18px] bg-black sm:h-20"
+    >
+      <p className="sm:mt-[20px]">
+        &copy; Copyright 2023 - Lucas Gabryel | NSoftwares
+      </p>
+    </footer>
+  );
+}
